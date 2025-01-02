eb92f01dbfd673de9d221509e48a3c32e783b64c
unknown <cheapp78@gmail.com>
2025-01-02 12:09:42 +0300

    my commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import axios from "axios";
 import { Button } from "./components/ui/button";
