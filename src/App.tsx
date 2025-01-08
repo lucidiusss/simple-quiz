b237660b495aab29b81ba62080cfc2559088da85
lucidiusss <cheapp78@gmail.com>
2025-01-08 20:10:25 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./index.css";
 import axios from "axios";
-import { Button, buttonVariants } from "./components/ui/button";
+import { Button } from "./components/ui/button";
 import toast from "react-hot-toast";
 
 interface QuizProps {
